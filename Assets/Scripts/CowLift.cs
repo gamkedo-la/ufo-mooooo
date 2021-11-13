fd79d2d7a6e559bc484b0ef8602ffa02f19178a6
cubnoble <cub.noble@yahoo.fr>
2021-11-12 20:24:14 -0600

    Cows moooooving!

--- Assets/Scripts/CowLift.cs
+++ Assets/Scripts/CowLift.cs
@@ -11,32 +11,70 @@ public class CowLift : MonoBehaviour
 
     bool isInGoal;
 
+    public bool canMove;
+    public float travelingSpeed;
+
+    bool isMoving;
+    float minStay = 3, maxStay = 15;
+    public float currentStay;
+    float minMove = 3, maxMove = 15;
+    public float currentMove;
+    bool hasPickedRotation;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        currentStay = Random.Range(minStay, maxStay);
+        transform.rotation = Quaternion.Euler(0,Random.Range(0, 360), 0);
+        currentMove = Random.Range(minMove, maxMove);
     }
 
     private void Update()
     {
         if (!isInGoal)
         {
-            if (isBeingLevitated)
+            if (!canMove)
             {
-                rb.velocity = transform.up * levitateSpeed;
+                if (isBeingLevitated)
+                {
+                    rb.velocity = transform.up * levitateSpeed;
+                }
+
+                if (hitTop)
+                {
+                    isBeingLevitated = false;
+                    rb.velocity = transform.up * 0;
+                }
+
+                if (Input.GetKeyUp(KeyCode.Space) && (hitTop || isBeingLevitated))
+                {
+                    isBeingLevitated = false;
+                    hitTop = false;
+                    rb.useGravity = true;
+                    this.transform.parent = null;
+                    canMove = true;
+                }
             }
 
-            if (hitTop)
+            if (canMove)
             {
-                isBeingLevitated = false;
-                rb.velocity = transform.up * 0;
-            }
+                currentMove -= Time.deltaTime;
+                if (isMoving)
+                {
+                    transform.Translate(Vector3.forward * Time.deltaTime * travelingSpeed);
+                }
 
-            if (Input.GetKeyUp(KeyCode.Space) && (hitTop || isBeingLevitated))
-            {
-                isBeingLevitated = false;
-                hitTop = false;
-                rb.useGravity = true;
-                this.transform.parent = null;
+                if (currentMove >= 0)
+                {
+                    isMoving = true;
+                }
+
+                else
+                {       
+                    isMoving = false;
+                    currentMove = 0;
+                    StartCoroutine(WaitForRotate());
+                }
             }
         }
 
@@ -45,6 +83,18 @@ public class CowLift : MonoBehaviour
             GetComponent<CowLift>().enabled = false;
             rb.constraints = RigidbodyConstraints.FreezeAll;
         }
+    }
+
+    IEnumerator WaitForRotate()
+    {
+        yield return new WaitForSeconds(currentStay);
+        if (!isMoving)
+        {
+            currentMove = Random.Range(minMove, maxMove);
+            currentStay = Random.Range(minStay, maxStay);
+            transform.rotation = Quaternion.Euler(0, Random.Range(0, 360), 0);
+        }
+        isMoving = true;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -56,6 +106,7 @@ public class CowLift : MonoBehaviour
                 rb.useGravity = false;
                 isBeingLevitated = true;
                 this.transform.parent = GameObject.FindGameObjectWithTag("Beam").transform;
+                canMove = false;
             }
 
             if (other.tag == "Stopper")
@@ -78,6 +129,7 @@ public class CowLift : MonoBehaviour
                 rb.useGravity = true;
                 isBeingLevitated = false;
                 this.transform.parent = null;
+                canMove = true;
             }
 
             if (other.tag == "Stopper")
