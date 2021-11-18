0b402955b78a309b6b2d284fc9edae4f8ceaea92
cubnoble <cub.noble@yahoo.fr>
2021-11-17 22:00:20 -0600

    Add Tree Colliders. Flip direction of LevelSelect ship

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -19,7 +19,7 @@ public class BasicMovement : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, -moveVertical);
         rb.AddForce(movement * speed);
     }
 
