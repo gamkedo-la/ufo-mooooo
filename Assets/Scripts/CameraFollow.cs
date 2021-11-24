eb79471bc993ada9ebdbc36395410b042b8b4244
Christer Kaitila <breakdance@mcfunkypants.com>
2021-11-24 15:31:35 -0800

    fixed a couple errors related to missing scripts and a prefab connection

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -11,12 +11,18 @@ public class CameraFollow : MonoBehaviour
 
     void Start()
     {
-        CamOffSet = Camera.transform.position - transform.position;
+        if (Camera!=null) {
+            CamOffSet = Camera.transform.position - transform.position;
+        } else {
+            Debug.Log("The CameraFollow component is missing a Camera. Set one in the inspector!");
+        }
     }
 
     void FixedUpdate()
     {
-       // transform.position += new Vector3(Input.GetAxis("Horizontal") * playerSpeed * Time.smoothDeltaTime, 0, Input.GetAxis("Vertical"));
-        Camera.transform.position = Vector3.Lerp(Camera.transform.position, transform.position + CamOffSet, Time.smoothDeltaTime * CameraFollowSpeed);
+        if (Camera!=null) {
+            // transform.position += new Vector3(Input.GetAxis("Horizontal") * playerSpeed * Time.smoothDeltaTime, 0, Input.GetAxis("Vertical"));
+            Camera.transform.position = Vector3.Lerp(Camera.transform.position, transform.position + CamOffSet, Time.smoothDeltaTime * CameraFollowSpeed);
+        }
     }
 }
