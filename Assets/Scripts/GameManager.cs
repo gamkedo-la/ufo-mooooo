8a363c7ccfc2b82ba8d3286a381ad8ce3085ec14
cubnoble <cub.noble@yahoo.fr>
2021-12-20 18:36:57 -0600

    The start of save mechanics for unlocking path

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,6 +18,16 @@ public class GameManager : MonoBehaviour
     public static int highestLevel1Score, highestLevel2Score, highestLevel3Score, highestLevel4Score, 
                       highestLevel5Score, highestLevel6Score, highestLevel7Score, highestLevel8Score;
 
+    public static bool CtoF1, F1toF2, F2toC,
+                       CtoI1, I1toI2, I2toC,
+                       CtoD1, D1toD2, D2toC,
+                       CtoC1, C1toC2;
+
+    public static int VCtoF1, VF1toF2, VF2toC,
+                       VCtoI1, VI1toI2, VI2toC,
+                       VCtoD1, VD1toD2, VD2toC,
+                       VCtoC1, VC1toC2;
+
     public static bool gateOpened;
 
     public static Vector3 playerPreservedSpace;
@@ -46,6 +56,76 @@ public class GameManager : MonoBehaviour
         PlayerPrefs.SetInt("Level7High", highestLevel7Score);
         PlayerPrefs.SetInt("Level8High", highestLevel8Score);
 
+        if (CtoF1)
+        {
+            VCtoF1 = 1;
+        }
+
+
+        if (F1toF2)
+        {
+            VF1toF2 = 1;
+        }
+
+        if (F2toC)
+        {
+            VF2toC = 1;          
+        }
+
+        if (CtoI1)
+        {
+            VCtoI1 = 1;          
+        }
+
+        if (I1toI2)
+        {
+            VI1toI2 = 1;          
+        }
+
+        if (I2toC)
+        {
+            VI2toC = 1;         
+        }
+
+        if (CtoD1)
+        {
+            VCtoD1 = 1;         
+        }
+
+        if (D1toD2)
+        {
+            VD1toD2 = 1;           
+        }
+
+        if (D2toC)
+        {
+            VD2toC = 1;           
+        }
+
+        if (CtoC1)
+        {
+            VCtoC1 = 1;            
+        }
+
+        if (C1toC2)
+        {
+            VC1toC2 = 1;
+        }
+
+
+
+        PlayerPrefs.SetInt("VCtoF1", VCtoF1);
+        PlayerPrefs.SetInt("VF1toF2", VF1toF2);
+        PlayerPrefs.SetInt("VF2toC", VF2toC);
+        PlayerPrefs.SetInt("VCtoI1", VCtoI1);
+        PlayerPrefs.SetInt("VI1toI2", VI1toI2);
+        PlayerPrefs.SetInt("VI2toC", VI2toC);
+        PlayerPrefs.SetInt("VCtoD1", VCtoD1);
+        PlayerPrefs.SetInt("VD1toD2", VD1toD2);
+        PlayerPrefs.SetInt("VD2toC", VD2toC);
+        PlayerPrefs.SetInt("VCtoC1", VCtoC1);
+        PlayerPrefs.SetInt("VC1toC2", VC1toC2);
+
         print("Game Saved: " + highestLevel1Score);
     }
 
@@ -59,6 +139,73 @@ public class GameManager : MonoBehaviour
         highestLevel6Score = PlayerPrefs.GetInt("Level6High");
         highestLevel7Score = PlayerPrefs.GetInt("Level7High");
         highestLevel8Score = PlayerPrefs.GetInt("Level8High");
+
+        VCtoF1 = PlayerPrefs.GetInt("VCtoF1");
+        VF1toF2 = PlayerPrefs.GetInt("VF1toF2");
+        VF2toC = PlayerPrefs.GetInt("VF2toC");
+        VCtoI1 = PlayerPrefs.GetInt("VCtoI1");
+        VI1toI2 = PlayerPrefs.GetInt("VI1toI2");
+        VI2toC = PlayerPrefs.GetInt("VI2toC");
+        VCtoD1 = PlayerPrefs.GetInt("VCtoD1");
+        VD1toD2 = PlayerPrefs.GetInt("VD1toD2");
+        VD2toC = PlayerPrefs.GetInt("VD2toC");
+        VCtoC1 = PlayerPrefs.GetInt("VCtoC1");
+        VC1toC2 = PlayerPrefs.GetInt("VC1toC2");
+
+        if (VCtoF1 == 1)
+        {
+            CtoF1 = true;
+        }
+
+        if (VF1toF2 == 1)
+        {
+            F1toF2 = true;
+        }
+
+        if (VF2toC == 1)
+        {
+            F2toC = true;
+        }
+
+        if (VCtoI1 == 1)
+        {
+            CtoI1 = true;
+        }
+
+        if (VI1toI2 == 1)
+        {
+            I1toI2 = true;
+        }
+
+        if (VI2toC == 1)
+        {
+            I2toC = true;
+        }
+
+        if (VCtoD1 == 1)
+        {
+            CtoD1 = true;
+        }
+
+        if (VD1toD2 == 1)
+        {
+            D1toD2 = true;
+        }
+
+        if (VD2toC == 1)
+        {
+            D2toC = true;
+        }
+
+        if (VCtoC1 == 1)
+        {
+            CtoC1 = true;
+        }
+
+        if (VC1toC2 == 1)
+        {
+            C1toC2 = true;
+        }
 
         print("Game Loaded: " + highestLevel1Score);
     }
--- Assets/TurnLevelOrbOn.cs
+++ Assets/TurnLevelOrbOn.cs
@@ -6,6 +6,11 @@ public class TurnLevelOrbOn : MonoBehaviour
 {
     public GameObject[] orbs;
 
+    public bool CtoF1, F1toF2, F2toC,
+                CtoI1, I1toI2, I2toC,
+                CtoD1, D1toD2, D2toC,
+                CtoC1, C1toC2;
+
     private void Start()
     {
         StartCoroutine(TurnOn());
@@ -17,6 +22,61 @@ public class TurnLevelOrbOn : MonoBehaviour
         {
             yield return new WaitForSeconds(1f);
             orbs[i].SetActive(true);
+        }
+
+        if (CtoF1)
+        {
+            GameManager.CtoF1 = true;
+        }
+
+        if (F1toF2)
+        {
+            GameManager.F1toF2 = true;
+        }
+
+        if (F2toC)
+        {
+            GameManager.F2toC = true;
+        }
+
+        if (CtoI1)
+        {
+            GameManager.CtoI1 = true;
+        }
+
+        if (I1toI2)
+        {
+            GameManager.I1toI2 = true;
+        }
+
+        if (I2toC)
+        {
+            GameManager.I2toC = true;
+        }
+
+        if (CtoD1)
+        {
+            GameManager.CtoD1 = true;
+        }
+
+        if (D1toD2)
+        {
+            GameManager.D1toD2 = true;
+        }
+
+        if (D2toC)
+        {
+            GameManager.D2toC = true;
+        }
+
+        if (CtoC1)
+        {
+            GameManager.CtoC1 = true;
+        }
+
+        if (C1toC2)
+        {
+            GameManager.C1toC2 = true;
         }
     }
 }
